dc93b9fb7645a8af64be6c8851d5c6060801e8f2
JulesCastel <122455844+JulesCastel@users.noreply.github.com>
2025-08-14 14:21:10 -0500

    start add prefixes selection

--- DC-available-seat/gui.py
+++ DC-available-seat/gui.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QLineEdit, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QComboBox, QLineEdit, QVBoxLayout, QWidget
 import sys
 
 class MainWindow(QMainWindow):
@@ -7,21 +7,39 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("Dallas College Course Availability Checker")
+        layout = QVBoxLayout()
 
-        button = QPushButton("Check")
-        button.setCheckable(True)
-        button.clicked.connect(self.clicked)
+        self.label = QLabel("Are there any open seats?")
+        layout.addWidget(self.label)
 
-        self.setFixedSize(QSize(400, 300))
+        input = QLineEdit()
+        input.setMaxLength(7)
+        input.setPlaceholderText("Enter Registration #")
+        layout.addWidget(input)
 
-        self.setCentralWidget(button)
+        self.dropdown = QComboBox()
+        layout.addWidget(self.dropdown)
 
-    def clicked(self):
-        print("clicked")
+        self.button = QPushButton("Check")
+        layout.addWidget(self.button)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+    def populateDropdown(self, filename: str):
+        with open("prefixes.txt", 'r') as file:
+            prefixes = file.readlines()
+            print(f"reading {file}")
+            for prefix in prefixes:
+                self.dropdown.addItem(prefix)
+
 
 app = QApplication(sys.argv)
 
 window = MainWindow()
+window.populateDropdown("prefixes.txt")
 window.show()
 
 app.exec()
