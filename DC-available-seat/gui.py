92a57db8a7432afcfed260e065576919faab045f
JulesCastel <122455844+JulesCastel@users.noreply.github.com>
2025-08-14 14:52:44 -0500

    give better variable names

--- DC-available-seat/gui.py
+++ DC-available-seat/gui.py
@@ -11,29 +11,29 @@ class MainWindow(QMainWindow):
         layout = QVBoxLayout()
 
         self.prefixLabel = QLabel("course prefix:")
-        self.dropdown = QComboBox()
+        self.prefixDropdown = QComboBox()
         layout.addWidget(self.prefixLabel)
-        layout.addWidget(self.dropdown)
+        layout.addWidget(self.prefixDropdown)
 
         self.regLabel = QLabel("registration #")
-        self.input = QLineEdit()
-        self.input.setMaxLength(7)
-        self.input.setPlaceholderText("enter registration #")
+        self.regInput = QLineEdit()
+        self.regInput.setMaxLength(7)
+        self.regInput.setPlaceholderText("enter registration #")
         layout.addWidget(self.regLabel)
-        layout.addWidget(self.input)
+        layout.addWidget(self.regInput)
 
-        self.button = QPushButton("check availability")
-        layout.addWidget(self.button)
+        self.checkButton = QPushButton("check availability")
+        layout.addWidget(self.checkButton)
 
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
 
-    def populateDropdown(self, filename: str):
-        self.dropdown.addItem("select course prefix")
-        # PyLance again being weird the hinting, this line is fine dammit!!
-        # PyQt gives the QComboBox (dropdown) its model() at runtime, so the type hinting is just wrong here
-        placeholder = self.dropdown.model().item(0) # type: ignore
+    def populatePrefixes(self, filename: str):
+        self.prefixDropdown.addItem("select course prefix")
+        # PyLance again being weird with the hinting, this line is fine dammit!!
+        # PyQt gives the QComboBox (prefixDropdown) its model() at runtime, so the type hinting is just wrong here
+        placeholder = self.prefixDropdown.model().item(0) # type: ignore
         placeholder.setFlags(Qt.ItemFlag.NoItemFlags)
 
         with open(filename, 'r') as file:
@@ -42,9 +42,9 @@ class MainWindow(QMainWindow):
                 if not text:
                     continue
 
-                self.dropdown.addItem(text)
-                idx = self.dropdown.count() - 1
-                prefix = self.dropdown.model().item(idx) # type: ignore
+                self.prefixDropdown.addItem(text)
+                idx = self.prefixDropdown.count() - 1
+                prefix = self.prefixDropdown.model().item(idx) # type: ignore
 
                 if any(char.islower() for char in text):
                     prefix.setFlags(Qt.ItemFlag.NoItemFlags)
@@ -56,7 +56,7 @@ class MainWindow(QMainWindow):
 app = QApplication(sys.argv)
 
 window = MainWindow()
-window.populateDropdown("prefixes.txt")
+window.populatePrefixes("prefixes.txt")
 window.show()
 
 app.exec()
